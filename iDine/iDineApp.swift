eae7b23a0a5484398e70d1cdf0fca33021849af3
Nicholas Gilbert <nicholasgilbert21@yahoo.com>
2023-05-14 23:12:07 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Nicholas Gilbert on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
